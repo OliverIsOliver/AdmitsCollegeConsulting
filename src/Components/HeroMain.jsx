a12208128cdc535cb5558a397770304b584a0905
greenoliver <66330061+greenoliver@users.noreply.github.com>
2026-03-13 20:12:03 -0500

    Made hero gradient hidden on mobile

--- src/Components/HeroMain.jsx
+++ src/Components/HeroMain.jsx
@@ -15,7 +15,7 @@ export default function HeroMain() {
         <>
         <div id="hero" className="relative isolate px-6 pt-14 lg:px-8">
           <div
-            className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+            className="hidden sm:block absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
             aria-hidden="true"
           >
             <div
